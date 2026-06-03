53726070571f598b95257c473ee886f848931ab5
Ruther.f <robelfikadu70@gmail.com>
2026-06-03 10:19:17 +0300

    chore: database configuration

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module';
+import { TestController } from './test/test.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [FirebaseModule],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
